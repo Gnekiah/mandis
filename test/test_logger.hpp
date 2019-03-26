b7a1a589e43adb88fd4ba69b7ad3e8281da94df7
Xxiong <xxiong@cqu.edu.cn>
2019-03-26 19:28:59 +0800

    test: test for ssllib::RsaPair and ssllib::RsaPubkey

--- test/test_logger.hpp
+++ test/test_logger.hpp
@@ -1,24 +1,14 @@
 #ifndef MANDIS_TEST_LOGGER_HPP_
 #define MANDIS_TEST_LOGGER_HPP_
 
-#include <boost/filesystem.hpp>
-
 #include "../include/logger.h"
-#include "../include/timelib.h"
 
-bool TestLoggerCase1() {
-    timelib::Time time;
-    std::string log_path = (boost::filesystem::temp_directory_path() / "mandis.log").string();
-    logger::Logger *logger = new logger::Logger(log_path, LOG_LEVEL_TRACE);
-    logger->Start();
-    LOG_ERROR(logger, "Error");
-    LOG_WARNING(logger, "Warning");
-    LOG_INFO(logger, "Information");
-    LOG_DEBUG(logger, "Debug");
-    LOG_TRACE(logger, "Trace");
-    time.MilliSleep(100);
-    logger->Stop();
-    logger->Join();
+bool TestLoggerCase1(logger::Logger *log) {
+    LOG_ERROR(log, "Error");
+    LOG_WARNING(log, "Warning");
+    LOG_INFO(log, "Information");
+    LOG_DEBUG(log, "Debug");
+    LOG_TRACE(log, "Trace");
     return 0;
 }
 
