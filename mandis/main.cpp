6dace41ce1a5eb1891cbfd80c91aa18d66e1bf6b
Xxiong <xxiong@cqu.edu.cn>
2019-03-26 09:19:10 +0800

    main: update mandis pro.

--- mandis/main.cpp
+++ mandis/main.cpp
@@ -1,18 +1,9 @@
 #include <iostream>
 
-#include <boost/version.hpp>
-#include <boost/config.hpp>
-
-
 int main(int argc, char **argv)
 {
 
-    std::cout << "Boost 版本:" << BOOST_VERSION << std::endl;
-    std::cout << "Boost lib 版本:" << BOOST_LIB_VERSION << std::endl;
-    std::cout << "操作系统:" << BOOST_PLATFORM << std::endl;
-    std::cout << "编译器:" << BOOST_COMPILER << std::endl;
-    std::cout << "标准库:" << BOOST_STDLIB << std::endl;
-    system("pause");
+    
     return 0;
 }
  
