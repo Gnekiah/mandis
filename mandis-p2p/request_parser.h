7ae7203d8a7e6a95736c0d486b7089497c827eb1
Xxiong <xxiong@cqu.edu.cn>
2019-04-02 15:41:06 +0800

    request_parser: parse request from remote, called by request_handler

---
+++ mandis-p2p/request_parser.h
@@ -0,0 +1,22 @@
+#ifndef MANDIS_MANDIS_P2P_REQUEST_PARSER_H_
+#define MANDIS_MANDIS_P2P_REQUEST_PARSER_H_
+
+namespace p2pnet {
+    struct request;
+
+    class RequestParser {
+    public:
+        RequestParser();
+
+        void Reset();
+
+    private:
+
+         
+    };
+
+
+}
+
+#endif // !MANDIS_MANDIS_P2P_REQUEST_PARSER_H_
+
