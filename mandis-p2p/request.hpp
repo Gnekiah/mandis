f3a5dce7ad98fdbce8353a968087d4b62ca6237f
Xxiong <xxiong@cqu.edu.cn>
2019-04-02 15:39:18 +0800

    request: define request structure

---
+++ mandis-p2p/request.hpp
@@ -0,0 +1,14 @@
+#ifndef MANDIS_MANDIS_P2P_REQUEST_HPP_
+#define MANDIS_MANDIS_P2P_REQUEST_HPP_
+
+namespace p2pnet {
+
+    typedef struct Request {
+
+    } Request;
+
+}
+
+
+#endif // !MANDIS_MANDIS_P2P_REQUEST_HPP_
+
