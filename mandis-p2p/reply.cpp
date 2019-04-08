5c2246522dc10ee34228bd78adc9ccc6e663090c
Xxiong <xxiong@cqu.edu.cn>
2019-04-08 15:23:11 +0800

    update proj

--- mandis-p2p/reply.cpp
+++
@@ -1,3 +0,0 @@
-namespace p2pnet {
-
-}
--- mandis-p2p/reply.h
+++
@@ -1,9 +0,0 @@
-#ifndef MANDIS_MANDIS_P2P_REPLY_H_
-#define MANDIS_MANDIS_P2P_REPLY_H_
-
-namespace p2pnet {
-    
-}
-
-#endif // !MANDIS_MANDIS_P2P_REPLY_H_
-
