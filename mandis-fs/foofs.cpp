244c7dfe48e529a965f9702289d23159468d8d67
Xxiong <xxiong@cqu.edu.cn>
2019-05-10 10:50:09 +0800

    mandis-fs: add module: mandis file subsystem

---
+++ mandis-fs/foofs.cpp
@@ -0,0 +1,29 @@
+#include "../include/foofs.h"
+
+namespace foofs {
+
+    FooFS::FooFS() {
+
+    }
+
+    FooFS::~FooFS() {
+
+    }
+
+    void FooFS::Write() {
+
+    }
+
+    void FooFS::Read() {
+
+    }
+    
+    void FooFS::Delete() {
+
+    }
+
+    void FooFS::ReadMetaData() {
+
+    }
+
+}
