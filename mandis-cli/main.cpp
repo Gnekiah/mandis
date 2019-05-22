bc37d79eec186a7d4555e46f1d3bc61cab96aab1
Xxiong <xxiong@cqu.edu.cn>
2019-05-22 22:15:56 +0800

    mandis-cli: command line interface

--- mandis-cli/main.cpp
+++ mandis-cli/main.cpp
@@ -2,196 +2,63 @@
 #include <string>
 #include <boost/asio.hpp>
 #include <boost/array.hpp>
-
-
-
-
-
-#include <cstdlib>
 #include <boost/bind.hpp>
-#include <boost/thread/thread.hpp>
+#include <boost/enable_shared_from_this.hpp>
 
-
-
-
-class Session {
+class Session : public boost::enable_shared_from_this<Session> {
 public:
-    Session(boost::asio::io_context& ioc, const boost::asio::ip::tcp::endpoint& ep)
+    Session(boost::asio::io_context& ioc, const boost::asio::ip::tcp::endpoint& ep, std::string& msg)
         : ioc_(ioc),
-        socket_(ioc) {
-        boost::asio::async_connect(socket_, ep, boost::bind(&Session::HandleConnect, this, 
+        socket_(ioc),
+        msg_(msg) {
+        socket_.async_connect(ep, boost::bind(&Session::HandleConnect, this,
             boost::asio::placeholders::error));
     }
 
 private:
     void HandleConnect(const boost::system::error_code& ec) {
+        if (ec) {
+            std::cerr << "Fatal: Error on HandleConnect" << std::endl;
+            std::cerr << ec.message() << std::endl;
+            return;
+        }
+        boost::asio::async_write(socket_, boost::asio::buffer(msg_.data(), msg_.length()), 
+            boost::bind(&Session::HandleWrite, this, boost::asio::placeholders::error));
+    }
 
+    void HandleWrite(const boost::system::error_code& ec) {
+        if (ec) {
+            std::cerr << "Fatal: Error on HandleWrite" << std::endl;
+            return;
+        }
+        boost::asio::async_read(socket_, boost::asio::buffer(buffer_.data(), 1024 * 512),
+            boost::bind(&Session::HandleRead, shared_from_this(), boost::asio::placeholders::error,
+                boost::asio::placeholders::bytes_transferred));
+    }
+
+    void HandleRead(const boost::system::error_code& ec, std::size_t bytes_transferred) {
+        if (ec) {
+            std::cerr << "Fatal: Error on HandleRead" << std::endl;
+            return;
+        }
     }
 
 private:
+    std::string &msg_;
     boost::asio::io_context& ioc_;
     boost::asio::ip::tcp::socket socket_;
     size_t buffer_length_;
     boost::array<unsigned char, 1024 * 512> buffer_;
 };
 
-class chat_client
-{
-public:
-    void write(const chat_message& msg)
-    {
-        boost::asio::post(io_context_,
-            boost::bind(&chat_client::do_write, this, msg));
-    }
-
-    void close()
-    {
-        boost::asio::post(io_context_,
-            boost::bind(&chat_client::do_close, this));
-    }
-
-private:
-
-    void handle_connect(const boost::system::error_code& error)
-    {
-        if (!error)
-        {
-            boost::asio::async_read(socket_,
-                boost::asio::buffer(read_msg_.data(), chat_message::header_length),
-                boost::bind(&chat_client::handle_read_header, this,
-                    boost::asio::placeholders::error));
-        }
-    }
-
-    void handle_read_header(const boost::system::error_code& error)
-    {
-        if (!error && read_msg_.decode_header())
-        {
-            boost::asio::async_read(socket_,
-                boost::asio::buffer(read_msg_.body(), read_msg_.body_length()),
-                boost::bind(&chat_client::handle_read_body, this,
-                    boost::asio::placeholders::error));
-        }
-        else
-        {
-            do_close();
-        }
-    }
-
-    void handle_read_body(const boost::system::error_code& error)
-    {
-        if (!error)
-        {
-            std::cout.write(read_msg_.body(), read_msg_.body_length());
-            std::cout << "\n";
-            boost::asio::async_read(socket_,
-                boost::asio::buffer(read_msg_.data(), chat_message::header_length),
-                boost::bind(&chat_client::handle_read_header, this,
-                    boost::asio::placeholders::error));
-        }
-        else
-        {
-            do_close();
-        }
-    }
-
-    void do_write(chat_message msg)
-    {
-        bool write_in_progress = !write_msgs_.empty();
-        write_msgs_.push_back(msg);
-        if (!write_in_progress)
-        {
-            boost::asio::async_write(socket_,
-                boost::asio::buffer(write_msgs_.front().data(),
-                    write_msgs_.front().length()),
-                boost::bind(&chat_client::handle_write, this,
-                    boost::asio::placeholders::error));
-        }
-    }
-
-    void handle_write(const boost::system::error_code& error)
-    {
-        if (!error)
-        {
-            write_msgs_.pop_front();
-            if (!write_msgs_.empty())
-            {
-                boost::asio::async_write(socket_,
-                    boost::asio::buffer(write_msgs_.front().data(),
-                        write_msgs_.front().length()),
-                    boost::bind(&chat_client::handle_write, this,
-                        boost::asio::placeholders::error));
-            }
-        }
-        else
-        {
-            do_close();
-        }
-    }
-
-    void do_close()
-    {
-        socket_.close();
-    }
-
-private:
-    boost::asio::io_context& io_context_;
-    boost::asio::ip::tcp::socket socket_;
-    chat_message read_msg_;
-    chat_message_queue write_msgs_;
-};
-
-int main(int argc, char* argv[])
-{
-
-        chat_client c(io_context, endpoints);
-        boost::thread t(boost::bind(&boost::asio::io_context::run, &io_context));
-
-        char line[chat_message::max_body_length + 1];
-        while (std::cin.getline(line, chat_message::max_body_length + 1))
-        {
-            using namespace std; // For strlen and memcpy.
-            chat_message msg;
-            msg.body_length(strlen(line));
-            memcpy(msg.body(), line, msg.body_length());
-            msg.encode_header();
-            c.write(msg);
-        }
-
-        c.close();
-        t.join();
-
-    return 0;
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 int DoConnect(std::string ops="", std::string filepath="", std::string filename="") {
     /// ops = list, writ, read
     boost::asio::io_context ioc;
     boost::asio::ip::tcp::endpoint ep(boost::asio::ip::address::from_string("127.0.0.1"), 60001);
-    Session session(ioc, ep);
-
-
+    std::string msg = ops + "|" + filepath + "|" + filename;
+    Session session(ioc, ep, msg);
+    ioc.run();
     return 0;
 }
 
