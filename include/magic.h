762d7924c83b80b05aa5564e0d4785b560caa452
ene <ene@xxiong.me>
2019-07-12 11:12:20 +0800

    fix atomic_bool construct bug

--- include/magic.h
+++ include/magic.h
@@ -4,7 +4,7 @@
 #include <atomic>
 
 /* While fatal error occurred, set the flag and the main thread'll exiting */
-static std::atomic_bool ExitFlag = false;
+static std::atomic_bool ExitFlag = { false };
 
 #endif // !MANDIS_MAGIC_H_
 
